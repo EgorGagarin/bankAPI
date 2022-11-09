49188083cf985dc5a9927cb971e6b52ce60d593d
Egor Gagarin <efgagarin@gmail.com>
2022-11-09 22:20:18 +0500

    feat: add userList to the UserController

--- src/main/java/com/gagarin/bankAPI/controller/UsersController.java
+++ src/main/java/com/gagarin/bankAPI/controller/UsersController.java
@@ -1,8 +1,12 @@
 package com.gagarin.bankAPI.controller;
 
+import com.gagarin.bankAPI.entity.User;
 import com.gagarin.bankAPI.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
@@ -13,4 +17,10 @@ public class UsersController {
     public UsersController(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping
+    public List<User> userList(){
+        return userService.userList();
+    }
+
 }
