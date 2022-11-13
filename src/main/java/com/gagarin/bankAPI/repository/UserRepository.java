ced4b6bef8fac566860804c73738eb48d87362f5
Egor Gagarin <efgagarin@gmail.com>
2022-11-13 21:26:48 +0500

    feat: add @Repository to the UserRepository

--- src/main/java/com/gagarin/bankAPI/repository/UserRepository.java
+++ src/main/java/com/gagarin/bankAPI/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.gagarin.bankAPI.repository;
 
 import com.gagarin.bankAPI.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
