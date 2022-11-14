6622999d298260b51b4bedb58922b3990ef6fcee
Egor Gagarin <efgagarin@gmail.com>
2022-11-14 23:26:05 +0500

    feat: add putMoneyUser to the OperationService

--- src/main/java/com/gagarin/bankAPI/service/OperationService.java
+++ src/main/java/com/gagarin/bankAPI/service/OperationService.java
@@ -1,19 +1,25 @@
 package com.gagarin.bankAPI.service;
 
+import com.gagarin.bankAPI.entity.OperationList;
 import com.gagarin.bankAPI.entity.User;
+import com.gagarin.bankAPI.repository.OperationListRepository;
 import com.gagarin.bankAPI.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
 public class OperationService {
 
     private final UserRepository userRepository;
+    private final OperationListRepository operationListRepository;
 
-    public OperationService(UserRepository userRepository) {
+    public OperationService(UserRepository userRepository, OperationListRepository operationListRepository) {
         this.userRepository = userRepository;
+        this.operationListRepository = operationListRepository;
     }
 
     public String getBalanceUser(Long userId) {
@@ -26,6 +32,34 @@ public class OperationService {
         } else {
             return "User with this id does not exist";
         }
+    }
+
+    @Transactional
+    public String putMoneyUser(Long userId, BigDecimal putMoney) {
+        Optional<User> row = userRepository.findById(userId);
+
+        if (row.isPresent()) {
+            User item = row.get();
+            if (putMoney.compareTo(BigDecimal.ZERO) > 0) {
+                BigDecimal currentBalance = item.getBalance();
+                BigDecimal newBalance = currentBalance.add(putMoney);
+                item.setBalance(newBalance);
+                userRepository.save(item);
+
+                String operation = "Contributed";
+
+                LocalDate date = LocalDate.now();
+
+                OperationList operationListAdd = new OperationList(operation, putMoney, date, item);
+                operationListRepository.save(operationListAdd);
+
+            } else {
+                return "Please enter a positive number greater than zero";
+            }
+        } else {
+            return "User with this id does not exist";
+        }
+        return "Added amount: " + putMoney;
     }
 
 }
