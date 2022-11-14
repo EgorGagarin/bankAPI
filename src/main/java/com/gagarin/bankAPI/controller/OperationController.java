ee8621dd7e19c476e21e50f329e6104feb1d184f
Egor Gagarin <efgagarin@gmail.com>
2022-11-14 23:33:20 +0500

    feat: add putMoneyUser to the OperationController

--- src/main/java/com/gagarin/bankAPI/controller/OperationController.java
+++ src/main/java/com/gagarin/bankAPI/controller/OperationController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 @RequestMapping(path = "operation")
 public class OperationController {
@@ -16,11 +18,15 @@ public class OperationController {
         this.operationService = operationService;
     }
 
-
     @GetMapping(path = "balance/{userId}")
     public String getBalanceUser(@PathVariable("userId") Long userId) {
         return operationService.getBalanceUser(userId);
     }
 
+    @GetMapping(path = "refill/{userId}/{putMoney}")
+    public String putMoneyUser(@PathVariable("userId") Long userId,
+                               @PathVariable("putMoney") BigDecimal putMoney) {
+        return operationService.putMoneyUser(userId, putMoney);
+    }
 
 }
