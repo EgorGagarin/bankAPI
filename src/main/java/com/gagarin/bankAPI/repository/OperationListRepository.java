6043464e33d605558bf0c41433d1f4ee34714806
Egor Gagarin <efgagarin@gmail.com>
2022-11-16 21:29:35 +0500

    feat: add @Repository to OperationListRepository

--- src/main/java/com/gagarin/bankAPI/repository/OperationListRepository.java
+++ src/main/java/com/gagarin/bankAPI/repository/OperationListRepository.java
@@ -2,6 +2,8 @@ package com.gagarin.bankAPI.repository;
 
 import com.gagarin.bankAPI.entity.OperationList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OperationListRepository extends JpaRepository<OperationList, Long> {
 }
