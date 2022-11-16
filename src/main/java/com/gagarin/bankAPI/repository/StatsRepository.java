1ba30f5dd324e665ef7ddc33d76517a81592902b
Egor Gagarin <efgagarin@gmail.com>
2022-11-16 21:45:59 +0500

    feat: add StatsRepository with getOperationList
    Getting a list of user operations by id with a filter by date

---
+++ src/main/java/com/gagarin/bankAPI/repository/StatsRepository.java
@@ -0,0 +1,32 @@
+package com.gagarin.bankAPI.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public class StatsRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public StatsRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Map<String, Object>> getOperationList(Long userId, LocalDate fromDate, LocalDate beforeDate) {
+
+        if (fromDate != null && beforeDate != null) {
+            return jdbcTemplate.queryForList("SELECT * FROM operation_list WHERE user_id= ? AND date_operation BETWEEN ? AND ?", userId, fromDate, beforeDate);
+
+        } else if (beforeDate == null && fromDate != null) {
+            return jdbcTemplate.queryForList("SELECT * FROM operation_list WHERE user_id= ? AND date_operation >= ?", userId, fromDate);
+
+        } else if (fromDate == null && beforeDate != null) {
+            return jdbcTemplate.queryForList("SELECT * FROM operation_list WHERE user_id= ? AND date_operation <= ?", userId, beforeDate);
+
+        } else {
+            return jdbcTemplate.queryForList("SELECT * FROM operation_list WHERE user_id= ?", userId);
+        }
+    }
+
+}
