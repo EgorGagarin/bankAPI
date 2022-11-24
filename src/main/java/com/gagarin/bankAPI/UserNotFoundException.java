8d72a12269d64a60c3ac8b04d269546d71ac5560
Egor Gagarin <efgagarin@gmail.com>
2022-11-24 20:56:10 +0500

    feat: add UserNotFoundException

--- src/main/java/com/gagarin/bankAPI/UserNotFoundException.java
+++ src/main/java/com/gagarin/bankAPI/UserNotFoundException.java
@@ -1,2 +1,9 @@
-package com.gagarin.bankAPI;public class UserNotFoundExeption {
+package com.gagarin.bankAPI;
+
+public class UserNotFoundException extends RuntimeException{
+
+    UserNotFoundException(Long id){
+        super ("Could not find user " + id);
+    }
+
 }
