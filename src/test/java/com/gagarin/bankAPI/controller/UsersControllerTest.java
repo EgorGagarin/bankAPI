71092612a3adc8947462d27ee860bb640fb73b79
Egor Gagarin <efgagarin@gmail.com>
2022-12-19 23:12:54 +0500

    test: add getUser to UserControllerTest

--- src/test/java/com/gagarin/bankAPI/controller/UsersControllerTest.java
+++ src/test/java/com/gagarin/bankAPI/controller/UsersControllerTest.java
@@ -1,14 +1,21 @@
 package com.gagarin.bankAPI.controller;
 
+import com.gagarin.bankAPI.entity.User;
 import com.gagarin.bankAPI.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -24,8 +31,20 @@ class UsersControllerTest {
 
     @Test
     void userList() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/"))
+        this.mockMvc.perform(get("/users/"))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void getUser() throws Exception {
+        User user = new User(1L, "Aleks", BigDecimal.valueOf(1000));
+        when(userService.getUser(1l))
+                .thenReturn(EntityModel.of(user));
+        this.mockMvc.perform(get("/users/1").accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":1, \"username\":\"Aleks\", \"balance\":1000}"));
+
     }
 }
