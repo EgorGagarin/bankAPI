d483fd7f935c71f95a38b0942a3c8420af12050c
Egor Gagarin <efgagarin@gmail.com>
2023-01-18 18:55:14 +0500

    feat: add MessageResponse to security

---
+++ src/main/java/com/gagarin/bankAPI/security/pojo/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.gagarin.bankAPI.security.pojo;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) { this.message = message; }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
