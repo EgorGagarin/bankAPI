66a99d74b92bd492dd1eb3719fe2ac38104100df
Egor Gagarin <efgagarin@gmail.com>
2023-01-18 16:16:50 +0500

    feat: add SignupRequest to pojo securety
    Class for signup user (signup) request

---
+++ src/main/java/com/gagarin/bankAPI/security/pojo/SignupRequest.java
@@ -0,0 +1,43 @@
+package com.gagarin.bankAPI.security.pojo;
+
+import java.util.Set;
+
+/**
+ * Class for signup user (signup) request.
+ */
+public class SignupRequest {
+    private String username;
+    private String email;
+    private Set<String> roles;
+    private String password;
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
