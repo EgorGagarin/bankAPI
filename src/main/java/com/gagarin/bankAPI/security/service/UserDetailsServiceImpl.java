b45a7cc1268a6894038d1f326a6d90d5271003db
Egor Gagarin <efgagarin@gmail.com>
2023-01-19 11:28:17 +0500

    feat: add UserDetailsServiceImpl to security

---
+++ src/main/java/com/gagarin/bankAPI/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.gagarin.bankAPI.security.service;
+
+import com.gagarin.bankAPI.entity.User;
+import com.gagarin.bankAPI.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Returns the username
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
