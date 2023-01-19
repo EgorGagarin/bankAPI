c250ba9c2f481f7681ca5ac4113f76723a85873f
Egor Gagarin <efgagarin@gmail.com>
2023-01-19 11:25:42 +0500

    feat: add JwtResponse to security

---
+++ src/main/java/com/gagarin/bankAPI/security/pojo/JwtResponse.java
@@ -0,0 +1,68 @@
+package com.gagarin.bankAPI.security.pojo;
+
+import java.util.Set;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private Set<String> roles;
+
+    public JwtResponse(String token, Long id, String username, String email, Set<String> roles) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
