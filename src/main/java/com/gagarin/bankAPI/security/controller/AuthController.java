d1969a8926ef4068d103ace79c6e0709f500f025
Egor Gagarin <efgagarin@gmail.com>
2023-01-20 14:54:48 +0500

    feat: add AuthController to security

---
+++ src/main/java/com/gagarin/bankAPI/security/controller/AuthController.java
@@ -0,0 +1,128 @@
+package com.gagarin.bankAPI.security.controller;
+
+import com.gagarin.bankAPI.security.jwt.JwtUtils;
+import com.gagarin.bankAPI.entity.ERole;
+import com.gagarin.bankAPI.entity.Role;
+import com.gagarin.bankAPI.entity.User;
+import com.gagarin.bankAPI.security.pojo.JwtResponse;
+import com.gagarin.bankAPI.security.pojo.LoginRequest;
+import com.gagarin.bankAPI.security.pojo.MessageResponse;
+import com.gagarin.bankAPI.security.pojo.SignupRequest;
+import com.gagarin.bankAPI.repository.RoleRepository;
+import com.gagarin.bankAPI.repository.UserRepository;
+import com.gagarin.bankAPI.security.service.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/auth/")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authUser(@RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        Set<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toSet());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest) {
+
+        if (userRepository.existsByUsername(signupRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is exist"));
+        }
+
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is exist"));
+        }
+
+        User user = new User(signupRequest.getUsername(),
+                signupRequest.getEmail(),
+                passwordEncoder.encode(signupRequest.getPassword()));
+
+        Set<String> reqRoles = signupRequest.getRoles();
+        Set<Role> roles = new HashSet<>();
+
+        if (reqRoles == null) {
+            Role userRole = roleRepository
+                    .findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error, Role USER is not found"));
+            roles.add(userRole);
+        } else {
+            reqRoles.forEach(r -> {
+                switch (r) {
+                    case "admin":
+                        Role adminRole = roleRepository
+                                .findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error, Role ADMIN is not found"));
+                        roles.add(adminRole);
+                        break;
+
+                    case "mod":
+                        Role modRole = roleRepository
+                                .findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error, Role MODERATOR is not found"));
+                        roles.add(modRole);
+                        break;
+
+                    default:
+                        Role userRole = roleRepository
+                                .findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error, Role USER is not found"));
+                        roles.add(userRole);
+                }
+            });
+        }
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User CREATED"));
+    }
+
+}
