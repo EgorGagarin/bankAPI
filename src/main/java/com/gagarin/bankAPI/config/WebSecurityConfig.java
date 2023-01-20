beabbe5551e7d0de85eaa352f3db3ba892620778
Egor Gagarin <efgagarin@gmail.com>
2023-01-20 16:56:37 +0500

    chore: deleted, MvcConfig UserConfiguration

--- src/main/java/com/gagarin/bankAPI/config/WebSecurityConfig.java
+++ src/main/java/com/gagarin/bankAPI/config/WebSecurityConfig.java
@@ -1,8 +1,8 @@
-package com.gagarin.bankAPI.config;
+package com.gagarin.bankAPI.security.config;
 
 import com.gagarin.bankAPI.security.jwt.AuthEntryPointJwt;
 import com.gagarin.bankAPI.security.jwt.AuthTokenFilter;
-import com.gagarin.bankAPI.security.UserDetailsServiceImpl;
+import com.gagarin.bankAPI.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -57,57 +57,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
 
-    /*private final DataSource dataSource;
-
-    public WebSecurityConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }*/
-
-    /*@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                    .logout()
-                    .permitAll();
-
-        return http.build();
-    }*/
-
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-    }*/
-
-    /*@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select username, password from user_balance where username=?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles from user_balance u inner join user_role ur on u.id = ur.user_id where u.username=?");
-    }*/
 
