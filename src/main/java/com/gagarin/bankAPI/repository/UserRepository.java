743f91dee83b28570dc1145035d81d7035085552
Egor Gagarin <efgagarin@gmail.com>
2023-01-20 21:05:26 +0500

    feat: add findByUsername, exists...

--- src/main/java/com/gagarin/bankAPI/repository/UserRepository.java
+++ src/main/java/com/gagarin/bankAPI/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.gagarin.bankAPI.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
