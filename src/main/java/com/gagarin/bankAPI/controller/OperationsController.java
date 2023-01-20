d117ed4b563c95c53503ca990231f5294c5133d2
Egor Gagarin <efgagarin@gmail.com>
2023-01-20 21:15:21 +0500

    feat: add /api/ to @RequestMapping

--- src/main/java/com/gagarin/bankAPI/controller/OperationsController.java
+++ src/main/java/com/gagarin/bankAPI/controller/OperationsController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "operation")
+@RequestMapping(path = "/api/operation")
 public class OperationsController {
 
     private final OperationService operationService;
