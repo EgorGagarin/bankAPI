161ee17d578a56e1cf6ca63a2e3cdcc3384cf08a
Egor Gagarin <efgagarin@gmail.com>
2023-01-24 00:09:24 +0500

    feat: change addUser SignupRequest to service

--- src/main/java/com/gagarin/bankAPI/service/UserService.java
+++ src/main/java/com/gagarin/bankAPI/service/UserService.java
@@ -1,15 +1,23 @@
 package com.gagarin.bankAPI.service;
 
 import com.gagarin.bankAPI.controller.UsersController;
+import com.gagarin.bankAPI.entity.ERole;
+import com.gagarin.bankAPI.entity.Role;
 import com.gagarin.bankAPI.entity.User;
+import com.gagarin.bankAPI.repository.RoleRepository;
 import com.gagarin.bankAPI.repository.UserRepository;
+import com.gagarin.bankAPI.security.pojo.MessageResponse;
+import com.gagarin.bankAPI.security.pojo.SignupRequest;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -20,10 +28,14 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserModelAssembler userModelAssembler;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
-    public UserService(UserRepository userRepository, UserModelAssembler userModelAssembler) {
+    public UserService(UserRepository userRepository, UserModelAssembler userModelAssembler, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.userModelAssembler = userModelAssembler;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
     }
 
     public CollectionModel<EntityModel<User>> userList() {
@@ -37,7 +49,59 @@ public class UserService {
                         .userList()).withSelfRel());
     }
 
-    public ResponseEntity<?> addUser(User user) {
+    public ResponseEntity<?> addUser(SignupRequest signupRequest) {
+
+            if (userRepository.existsByUsername(signupRequest.getUsername())) {
+                return ResponseEntity
+                        .badRequest()
+                        .body(new MessageResponse("Error: Username is exist"));
+            }
+
+            if (userRepository.existsByEmail(signupRequest.getEmail())) {
+                return ResponseEntity
+                        .badRequest()
+                        .body(new MessageResponse("Error: Email is exist"));
+            }
+
+            User user = new User(signupRequest.getUsername(),
+                    signupRequest.getEmail(),
+                    passwordEncoder.encode(signupRequest.getPassword()));
+
+            Set<String> reqRoles = signupRequest.getRoles();
+            Set<Role> roles = new HashSet<>();
+
+            if (reqRoles == null) {
+                Role userRole = roleRepository
+                        .findByName(ERole.ROLE_USER)
+                        .orElseThrow(() -> new RuntimeException("Error, Role USER is not found"));
+                roles.add(userRole);
+            } else {
+                reqRoles.forEach(r -> {
+                    switch (r) {
+                        case "admin":
+                            Role adminRole = roleRepository
+                                    .findByName(ERole.ROLE_ADMIN)
+                                    .orElseThrow(() -> new RuntimeException("Error, Role ADMIN is not found"));
+                            roles.add(adminRole);
+                            break;
+
+                        case "mod":
+                            Role modRole = roleRepository
+                                    .findByName(ERole.ROLE_MODERATOR)
+                                    .orElseThrow(() -> new RuntimeException("Error, Role MODERATOR is not found"));
+                            roles.add(modRole);
+                            break;
+
+                        default:
+                            Role userRole = roleRepository
+                                    .findByName(ERole.ROLE_USER)
+                                    .orElseThrow(() -> new RuntimeException("Error, Role USER is not found"));
+                            roles.add(userRole);
+                    }
+                });
+            }
+            user.setRoles(roles);
+
         EntityModel<User> entityModel = userModelAssembler.toModel(userRepository.save(user));
 
         return ResponseEntity
